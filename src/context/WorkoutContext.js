a53f15ba0021f86c309421e1f202dc00ff00f4df
sunilshrees <sunilshrees371@gmail.com>
2022-07-01 18:24:36 +0545

    initial commit

---
+++ src/context/WorkoutContext.js
@@ -0,0 +1,38 @@
+import { createContext, useReducer } from 'react';
+
+export const WorkoutsContext = createContext();
+
+export const WorkoutReducer = (state, action) => {
+    if (action.type === 'SET_WORKOUTS') {
+        return {
+            workouts: action.payload,
+        };
+    }
+
+    if (action.type === 'CREATE_WORKOUT') {
+        return {
+            workouts: [action.payload, ...state.workouts],
+        };
+    }
+    if (action.type === 'DELETE_WORKOUT') {
+        return {
+            workouts: state.workouts.filter(
+                (w) => w._id !== action.payload._id
+            ),
+        };
+    }
+
+    return state;
+};
+
+export const WorkoutsContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(WorkoutReducer, {
+        workouts: null,
+    });
+
+    return (
+        <WorkoutsContext.Provider value={{ ...state, dispatch }}>
+            {children}
+        </WorkoutsContext.Provider>
+    );
+};
---
+++ src/hooks/useWorkoutsContext.js
@@ -0,0 +1,13 @@
+import { WorkoutsContext } from '../context/WorkoutContext';
+
+import { useContext } from 'react';
+
+export const useWorkoutsContext = () => {
+    const context = useContext(WorkoutsContext);
+
+    if (!context) {
+        throw Error('some error ');
+    }
+
+    return context;
+};
